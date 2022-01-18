f4211a906054b1b35397af6005a030c0e2eb16ef
yayausma <yayausma@fit.cvut.cz>
2022-01-19 02:36:46 +0300

    configuring the plugin for relative path or nested path

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,35 @@ function App() {
   const ref = useRef<any>();
 
   const startService = async () => {
-    const service = await esbuild.initialize({
+    ref.current= await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm'
     })
+    
+    
   }
 
   useEffect(() => {
     startService()
   }, [])
 
-  const handleSubmit = () =>{
+  const handleSubmit = async () =>{
+    if (!ref.current) {
+      return;
+    }
 
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result.outputFiles[0].text);
   }
   return (
     <div className="App">
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,21 +1,57 @@
-import * as esbuild from 'esbuild-wasm'
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
-    return{
-    name: 'unpkg-path-plugin',
+  return {
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-    build.onResolve({ filter: /.*/ }, async (args: any) => ({
-        path: args.path,
-        namespace: 'a',
-      }))
-  
-      build.onLoad({ filter: /.*/, namespace: 'a' }, () => ({
-        contents: JSON.stringify(process.env),
-        loader: 'json',
-      }))
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            path: args.path,
+            namespace: "a",
+          };
+        }
+
+        
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
+        }
+
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: "a",
+        };
+      });
+
+
+
+      build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `import message from './message
+                      console.log(message)
+            `,
+          };
+        }
+
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+      });
     },
-  }
-  
+  };
+
   // require('esbuild').build({
   //   entryPoints: ['index.js'],
   //   bundle: true,
@@ -23,4 +59,4 @@ export const unpkgPathPlugin = () => {
   //   // outfile: 'out.js',
   //   plugins: [unpkgPathPlugin()],
   // }).catch(() => process.exit(1))
-}
+};
